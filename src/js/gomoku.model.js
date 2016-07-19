2be35221aefc3c58b27d3f9e72df9b7814e6ad78
Drm1804 <drm-a@yandex.ru>
2016-07-19 15:35:53 +0500

    Сделал заготовку файлов и прописал к ним примерный функционал

---
+++ src/js/gomoku.model.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*
+ * Модуль gomoku.view, отвечает за логику игры
+ *
+ * Предназначение:
+ *   - todo инициализация controller и view
+ *   - todo хранение настроек игры
+ *   - todo алгоритм AI
+ *
+ * */
+
+gomoku.model = (function(){
+
+    function initModule(){
+
+    }
+
+    return{
+        initModule:initModule
+    }
+})();
---
+++ src/js/gomoku.view.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*
+* Модуль gomoku.view, отвечает за отображение информации
+*
+* Предназначение:
+*   - todo отображение приветсвенного окна
+*   - todo построение и отображение игрового поля
+*   - todo показ результатов ингы
+*
+* */
+
+gomoku.view = (function(){
+
+    function initModule(){
+
+    }
+
+    return{
+        initModule:initModule
+    }
+})();
