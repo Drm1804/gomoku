b4f1a59c1db785007ed899b9923c8d032f14b8fa
Drm1804 <drm-a@yandex.ru>
2016-07-19 13:29:59 +0500

    Добавил livereloader, добавил сборку для разработки

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,32 @@
 var gulp = require('gulp');
 // var conf = require('./conf');
 var browserSync = require('browser-sync').create();
+var inject = require('gulp-inject');
+var copy = require('gulp-copy');
+// var start = require('gulp-start');
+
+gulp.task('copy-temp', function () {
+    return gulp.src(['./src/**/*', '!./src/index.html'])
+        .pipe(copy('./.tmp'));
+});
+
+gulp.task('inject', ['copy-temp'], function () {
+    var target = gulp.src('./src/index.html');
+    var sources = gulp.src(['./src/**/*.js', './src/**/*.css'], {read: false});
+
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest('./.tmp'))
+        .pipe(browserSync.stream());
+});
 
 
-gulp.task('serve', function(){
+gulp.task('serve', ['inject'], function(){
 
     browserSync.init({
         server: {
-            baseDir :'./src'
+            baseDir :'./.tmp'
         }
     });
 
-    gulp.watch("src/js/*.js").on('change', browserSync.reload);
-    gulp.watch("src/css/*.css").on('change', browserSync.reload);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(["src/css/*.css", "src/js/*.js", "src/*.html"], ['inject']);
 });
