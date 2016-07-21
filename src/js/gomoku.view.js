a81008c091a20fb4ca388e45e01dd3deab79aa57
Drm1804 <drm-a@yandex.ru>
2016-07-21 17:04:23 +0500

    Начал переделывать структуру приложения

--- src/js/gomoku.view.js
+++ src/js/gomoku.view.js
@@ -7,11 +7,11 @@
  *
  * */
 
-G.View = function (_model, _rootObject) {
+G.View = function (_model, rootObject) {
 
     var that = this;
     var model = _model;
-    var rootObject = _rootObject;
+    var jqMap = {};
     that.createHtmlField = createHtmlField;
     that.createHtmlPauseWindow = createHtmlPauseWindow;
     that.createHtmlStartWindow = createHtmlStartWindow;
@@ -24,6 +24,7 @@ G.View = function (_model, _rootObject) {
      */
 
     function run(){
+        jqMap.rootObject = rootObject;
         that.createHtmlStartWindow();
     }
 
@@ -46,21 +47,26 @@ G.View = function (_model, _rootObject) {
             '<label for="g-start-size-input">Величина игрового поля</label>' +
             '<input type="number" value="20" id="g-start-size-input" class="g-start-size-input">' +
             '<h3>Противник:</h3>' +
-            '<input style="display:none" class="g-start-radio-input" type="radio" value="man" name="opponent" id="g-start-opponent-man">' +
+            '<input style="display:none" class="g-start-radio-input g-start-radio-input--opponent" type="radio" value="man" name="opponent" id="g-start-opponent-man">' +
             '<label class="g-start-op-man-label" for="g-start-opponent-man"></label>' +
-            '<input style="display:none" class="g-start-radio-input" type="radio" value="pc" name="opponent" id="g-start-opponent-pc">' +
+            '<input style="display:none" class="g-start-radio-input g-start-radio-input--opponent" type="radio" value="pc" name="opponent" id="g-start-opponent-pc">' +
             '<label class="g-start-op-pc-label" for="g-start-opponent-pc"></label>' +
+            '<div class="g-start-type-box">' +
             '<h3>Играть за:</h3>' +
-            '<input style="display:none" type="radio" class="g-start-radio-input" id="g-start-type-x" name="typeFigure">' +
+            '<input style="display:none" type="radio" class="g-start-radio-input g-start-radio-input--type" id="g-start-type-x" name="typeFigure">' +
             '<label class="g-start-type-x" for="g-start-type-x">X</label>' +
-            '<input style="display:none" type="radio" class="g-start-radio-input" id="g-start-type-o" name="typeFigure">' +
+            '<input style="display:none" type="radio" class="g-start-radio-input g-start-radio-input--type" id="g-start-type-o" name="typeFigure">' +
             '<label class="g-start-type-o" for="g-start-type-o">O</label>' +
+            '</div>' +
             '<button class="g-start-run-game">Начать игру</button>' +
             '</div>' +
             '</div>';
 
-        rootObject.append(html);
-
+        jqMap.rootObject.append(html);
+        jqMap.startTypeBox = rootObject.find('.g-start-type-box');
+        jqMap.startCheckOpponent = rootObject.find('.g-start-radio-input--opponent');
+        jqMap.startCheckTypeGame = rootObject.find('.g-start-radio-input--type');
+        jqMap.btnRunGame = rootObject.find('.g-start-run-game')
     }
 
     function createHtmlField(size) {
